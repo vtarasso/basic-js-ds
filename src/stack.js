9d4dc015e4bdb392cb658441b472d7e68029b0d3
Vitaliy Tarassov <apxustudio@gmail.com>
2023-12-25 15:42:51 +0600

    feat: stack done

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.elements = [];
+  }
+
+  push( element ) {
+    this.elements.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.elements.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const length = this.elements.length;
+    if (length > 0) {
+      return this.elements[length - 1];
+    } else {
+      return undefined;
+    }
   }
 }
 
